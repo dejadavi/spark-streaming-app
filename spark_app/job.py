ef63bbe815be69a311dd998eb4ff8b69a0c0798a
Desmond Davis <dejadavi@MacBook-Pro.local>
2021-12-20 16:41:56 -0600

    remove idea files

--- spark_app/job.py
+++ spark_app/job.py
@@ -2,7 +2,9 @@ from pyspark.sql import SparkSession
 from pyspark.sql import DataFrame
 from pyspark.sql.streaming import  DataStreamReader, StreamingQuery, StreamingQueryManager
 from pyspark.sql.functions import explode, col, date_format, current_date
-from .schema import NvdSchema
+from schema import NvdSchema
+from typing import Callable
+from functools import partial
 
 
 class Pipeline(object):
@@ -16,6 +18,7 @@ class Pipeline(object):
             .master("local[*]")\
             .config("spark.jars.packages",
                     "org.apache.spark:spark-sql-kafka-0-10_2.12:3.1.0") \
+            .enableHiveSupport()\
             .getOrCreate()
 
         return spark
@@ -26,10 +29,11 @@ class Pipeline(object):
              path: str) -> DataFrame:
 
         raw: DataStreamReader = spark.readStream \
-            .format("json")\
-            .option("schema", NvdSchema)\
-            .option("mode", "FAILFAST")\
-            .load(path)
+            .json(path=path,
+                  schema=NvdSchema,
+                  mode="FAILFAST")
+
+        raw.printSchema()
 
         return raw
 
@@ -40,34 +44,50 @@ class Pipeline(object):
 
         transformed: StreamingQuery = datastream\
             .withColumn("CVE_Item", explode(col("CVE_Items")))\
-            .select(col("CVE_Item.*"))
+            .select(col("CVE_Item.*"),
+                    date_format(current_date(),
+                                "YYYY-mm-dd").alias("dw_date"))
 
         transformed.printSchema()
 
         return transformed
+
+    @staticmethod
+    def writer(spark: SparkSession,
+               df: DataFrame)->Callable[[DataFrame, str], None]:
+
+        def batch_writer( df:DataFrame, epoch_id: str)->None:
+
+            spark.logger.info(epoch_id)
+
+            df\
+                .format("orc") \
+                .outputMode("overwrite") \
+                .partitionBy(["dw_date",]) \
+                .saveAsTable("prod.cve_data_by_hour")
+
+        return batch_writer()
 
     @classmethod
     def write(cls,
               spark: SparkSession,
               query: StreamingQuery) -> StreamingQueryManager:
 
-        query.start()
+        query.printSchema()
 
-        query\
-            .writeStream\
-            .format("orc")\
-            .partitionBy(date_format(col(current_date()), "YYYY-mm-dd"))\
-            .saveAsTable("prod.cve_data_by_hour")
+        query_cts : StreamingQueryManager = query.writeStream\
+            .foreachBatch(partial(cls.writer, spark))\
+            .start()
 
-        return query
+        return query_cts
 
     @classmethod
     def run(cls,
-            path: str = "/local/logs/*") -> DataFrame:
+            path: str = "/local/logs/*") -> StreamingQueryManager:
 
         spark: SparkSession = cls.get_spark()
         read: DataFrame = cls.read(spark, path)
-        transformed: DataFrame = cls.transform(spark,read)
+        transformed: DataFrame = cls.transform(spark, read)
         written: StreamingQueryManager = cls.write(spark, transformed)
 
         return written
